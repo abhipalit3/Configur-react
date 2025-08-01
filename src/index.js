6770c14163076fb193768aa07820f48e9db499a1
Abhishek Palit <arabhishek101@gmail.com>
2025-08-01 15:00:30 -0700

    updated button links and github pages integration

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
-} from 'react-router-dom'
+} from 'react-router-dom';
 
-import './style.css'
-import ProjectDashboard from './views/project-dashboard'
-import Projects from './views/projects'
-import Home from './views/home'
-import AppPage from './views/app-page'
-import SignupPage from './views/signup-page'
-import Login from './views/login'
-import NotFound from './views/not-found'
+import './style.css';
+import ProjectDashboard from './views/project-dashboard';
+import Projects from './views/projects';
+import Home from './views/home';
+import AppPage from './views/app-page';
+import SignupPage from './views/signup-page';
+import Login from './views/login';
+import NotFound from './views/not-found';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/Configur-react">
       <Switch>
         <Route component={ProjectDashboard} exact path="/project-dashboard" />
         <Route component={Projects} exact path="/projects" />
@@ -30,7 +30,7 @@ const App = () => {
         <Redirect to="**" />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
